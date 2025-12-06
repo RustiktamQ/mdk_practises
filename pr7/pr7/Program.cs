97b3351df4e7c9f8362f398c2baad446dc5cf516
RustiktamQ <rramilperm@gmail.com>
2025-12-06 16:15:53 +0500

    олучение информации с сайта. Рахмангулов

---
+++ pr7/pr7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+class Program
+{
+    const string urlMain = "http://news.permaviat-local.ru/main";
+
+    static void Main()
+    {
+        var request = (HttpWebRequest)WebRequest.Create(urlMain);
+        request.UserAgent = "Mozilla/5.0";
+
+        using var response = (HttpWebResponse)request.GetResponse();
+        Console.WriteLine($"Status: {response.StatusCode}");
+
+        using var stream = response.GetResponseStream();
+        using var reader = new StreamReader(stream);
+        string html = reader.ReadToEnd();
+        Console.WriteLine(html);
+
+        Console.Read();
+    }
+}
